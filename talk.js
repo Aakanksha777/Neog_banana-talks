9dbeb4d267c57081b2c94b8266d4dc3542dd304d
Aakanksha <109036438+Aakanksha777@users.noreply.github.com>
2022-12-14 18:04:45 +0530

    style : styling app

--- talk.js
+++ talk.js
@@ -1,16 +1,45 @@
-console.log("hi")
+//------Taking References----------------------------------------------------->>>>>>>>>
 const txt = document.getElementById('txt');
-console.log(txt);
-
 const btnTranslate = document.getElementById('btn-translate');
-console.log(btnTranslate);
+const outputDiv = document.getElementById("output");
+console.log(outputDiv)
+//-------------------------------------------------------------------------------------
+
+
+//------API-------------------------------------------------------------------->>>>>>>>>>
+var serverURL= "https://api.funtranslations.com/translate/minion.json"
+
+//-------------------------------------------------------------------------------------
+
+//----Function for 
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler (error) {
+    console.log("error occured", error);
+    alert("something wrong with server");
+}
 
 function translate() {
-    console.log('clicked')
-    console.log(txt.value)
+    // outputDiv.innerText = txt.value  --> not working
+    var inputTxt = txt.value; 
+    
+    //Calling server for processing:
+    fetch(getTranslationURL(inputTxt))
+    .then (response => response.json())
+    .then (json => {
+        var translatedText = json.contents.translated 
+        outputDiv.innerText = translatedText
+    })
+    .catch(errorHandler)
+    // .then (json => console.log(json))
 }
 
 btnTranslate.addEventListener('click', translate);
 
+
+
+// var mockURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 
